8ac7c4478c0a327fc71b973ad41e633ce889cd24
Camilo Forero <caforerof@gmail.com>
2019-11-10 17:26:41 -0500

    Final Readme.md

--- modules/vpc/07_rds.tf
+++ modules/vpc/07_rds.tf
@@ -12,17 +12,21 @@ resource "aws_db_instance" "rds_ma_postgres" {
   username             = "demouser"
   password             = "password"
   identifier           = "rds-ma-postgres"
+  snapshot_identifier  = "snap-id-postgres"
+  skip_final_snapshot  = true
 }
 
 #---------POSTGRES RDS DWH
 resource "aws_db_instance" "rds_ma_dwh" {
   allocated_storage    = 20
   storage_type         = "gp2"
-  engine               = "postgresql"
+  engine               = "postgres"
   engine_version       = "10.9"
   instance_class       = "${var.rds_instance_dwh_type}"
   name                 = "dwh_postgres_db"
   username             = "demouser"
   password             = "password"
-  identifier           = "rds-ma-postgres"
+  identifier           = "rds-ma-dwh"
+  snapshot_identifier  = "snap-id-dwh"
+  skip_final_snapshot  = true
 }
